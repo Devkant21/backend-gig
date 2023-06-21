3da687e7ef0a2f222b03489161d5e157981dc019
Devkant <devkant21@gmail.com>
2023-06-21 17:54:18 +0530

    commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import app from "./server";
+// import app from "./server";
 import mongodb from "mongodb"
 import dotenv from "dotenv"
 import NCDAO from "./dao/ncDAO";
@@ -6,23 +6,23 @@ import ReviewsDAO from './dao/reviewsDAO.js'
 
 async function main() {
     dotenv.config()
-        const client = new mongodb.MongoClient(
-            process.env.NESTCOLOURS_DB_URI
-        )
-        const port = process.env.PORT || 8000
+    const client = new mongodb.MongoClient(
+        process.env.NESTCOLOURS_DB_URI
+    )
+    const port = process.env.PORT || 8000
 
-        try {
-            // Connect to the MongoDB cluster
-            await client.connect()
-            await NCDAO.injectDB(client)
-            await ReviewsDAO.injectDB(client)
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect()
+        await NCDAO.injectDB(client)
+        await ReviewsDAO.injectDB(client)
 
-            app.listen(port, () => {
-                console.log('server is running on port:' + port);
-            })
-        } catch (e) {
-            console.error(e);
-            process.exit(1)
-        }
+        app.listen(port, () => {
+            console.log('server is running on port:' + port);
+        })
+    } catch (e) {
+        console.error(e);
+        process.exit(1)
     }
-        main().catch(console.error);
+}
+main().catch(console.error);
